14b3e08bde88eb0f46acfd8c19e7d1b968219ee4
bugra-ozer <bugraozer791@gmail.com>
2026-05-30 12:19:52 +0100

    refactor: add config and main modular units

---
+++ config.py
@@ -0,0 +1,22 @@
+# config.py
+
+APP_NAME = 'Strava Viewer'
+APP_ID = 'Strava Viewer'
+WINDOW_GEOMETRY = '1200x800'
+
+# Theme Colors
+BG_COLOR_MAIN = '#262624'
+TEXT_COLOR = '#FFFFFF'
+BUTTON_BG_COLOR = '#302F2E'
+ACCENT_COLOR = '#D8642E'
+FG_COLOR = '#E9E9E9'
+
+# Fonts
+FONT_MAIN = ('Segoe UI', 12)
+
+# Data Display Columns
+DEFAULT_COLUMNS = [
+    'activity id', 'activity date', 'moving time/h', 'distance',
+    'max heart rate', 'average heart rate', 'average speed kmh',
+    'max speed kmh', 'average watts', 'calories'
+]
